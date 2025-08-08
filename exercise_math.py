d6e853e8d5615d014303a005b718b34565adf6d1
LernerJoaquin <lernerjoaquin@gmail.com>
2025-08-08 12:07:53 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
     a = 57
     b = 7
+    suma = a + b
+    resta = a - b
+    producto = a * b
+    cuenta = (a + b) / 2
+    entero = a//b
+    resto = a % b
+    division = a / b
+    print(suma)
+    print(resta)
+    print(producto)
+    print(cuenta)
+    print(entero)
+    print(resto)
+    print(division)
